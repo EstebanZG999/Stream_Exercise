66956626e086738f55ad3f688080a298e52e92b8
Esteban Zambrano Garoz <zam22119@uvg.edu.gt>
2026-02-17 12:53:21 -0600

    feat: part 1 cipher and decipher completed

--- stream_cipher.py
+++ stream_cipher.py
@@ -6,3 +6,30 @@ def generar_keystream(seed: str, longitud: int) -> bytes:
     ks = bytes(prng.randrange(0, 256) for _ in range(longitud))
 
     return ks
+
+def cifrar(mensaje: str, clave: str) -> bytes:
+    mensaje_bytes = mensaje.encode('utf-8')
+
+    keystream = generar_keystream(clave, len(mensaje_bytes))
+
+    cifrado = bytes(m ^ k for m, k in zip(mensaje_bytes, keystream))
+
+    return cifrado
+
+def descifrar(cifrado: bytes, clave: str) -> str:
+    keystream = generar_keystream(clave, len(cifrado))
+
+    mensaje_bytes = bytes(c ^ k for c, k in zip(cifrado, keystream))
+
+    mensaje = mensaje_bytes.decode('utf-8')
+
+    return mensaje
+
+
+mensaje_original = "Hola mundo"
+clave = "mi_clave"
+
+c = cifrar(mensaje_original, clave)
+d = descifrar(c, clave)
+
+print(d == mensaje_original)
